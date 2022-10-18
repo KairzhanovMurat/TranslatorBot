efe18283c232e8ae036ec7d0258642082f8bfb66
Murat Kairzhanov <murat99010@gmail.com>
2022-10-18 17:24:20 +0600

    f

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import telebot
 import time
 from googletrans import Translator
+import os
 
 
 translator=Translator()
 
-bot=telebot.TeleBot('5361621189:AAFseIkJkfbpy34JRa1bjM_22jMkYkO_fCw')
+bot=telebot.TeleBot(os.getenv('token'))
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(chat_id=message.chat.id,text= f'Пашел наху {message.from_user.first_name}. Хули пишешь сюда.')
+    bot.send_message(chat_id=message.chat.id,text= f'Здарова {message.from_user.first_name}.')
     time.sleep(3)
-    bot.send_message(chat_id=message.chat.id, text='Шчу сайпал, пиши че нить а я буду переводить на анг. Не ебу зачем Мурат меня создал, ну да ладно...')
+    bot.send_message(chat_id=message.chat.id, text=' пиши че нить а я буду переводить на анг. ')
 
 
 @bot.message_handler()
